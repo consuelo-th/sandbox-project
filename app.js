1e28c2ae43dfee8e17f90e44cea3e5cdef8085c5
Hassan Yahya <hassanyahya400@gmail.com>
2022-11-07 23:17:16 +0100

    auth page, user layout (#26)
    * update mental health tips
    
    * added middlewares for handling sessions
    
    * added new schema for forum and forum comments, updated the user schema too
    
    * display the user's name
    
    * implemented session, fetch user details and send to the rendered template
    
    * installed modules needed for session
    
    * add new database uri, user blog page
    
    * create folder for th
    
    Co-authored-by: e-iroro <eruiroro18@gmail.com>
    Co-authored-by: qoudri4re <qoudri4re@gmail.com>

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ require("./database/connection");
 const express = require("express");
 const app = express();
 const usersRoutes = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const sessions = require("express-session");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.json());
-
+app.use(
+  sessions({
+    secret: "donttellanyone",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false,
+  })
+);
+app.use(cookieParser());
 // parse incoming post request's body
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/user", usersRoutes);
 
 app.get("/", function (req, res) {
@@ -21,7 +30,7 @@ app.get("/design", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.render("pages/errorPages/404");
+  res.render("pages/templates/404");
 });
 
 const PORT = process.env.PORT || 3000;
